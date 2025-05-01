04ba598695cf7c6153a210a8877686457ffc7957
Juan Tobar <juanpablococtm@gmail.com>
2025-04-30 22:46:08 -0500

    mejorar la conexión a MongoDB y ajustar la importación de mongoose

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
 export async function conectarDB() {
+  console.log("⏳ Conectando a MongoDB...");
   try {
-    await mongoose.connect("mongodb://localhost:27017/proyecto_big_data", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect("mongodb://127.0.0.1:27017/proyecto_big_data");
     console.log("✅ Conectado a MongoDB");
   } catch (error) {
-    console.error("❌ Error al conectar a MongoDB", error);
-    process.exit(1); // Detiene la app si falla la conexión
+    console.error("❌ Error al conectar a MongoDB:", error.message);
+    process.exit(1);
   }
 }
